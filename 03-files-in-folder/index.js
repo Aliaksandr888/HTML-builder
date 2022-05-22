5570d5ed8c4cfcf08c1f650ef3edfa4d68dbbd66
Aliaksandr <al_brain8@tut.by>
2022-05-22 17:34:34 +0300

    fix: 03-files-in-folder

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -3,15 +3,11 @@ const fs = require('fs');
 const { dirname } = require('path');
 //   node 03-files-in-folder
 
-console.log(path.join(__filename))
-
-
 fs.readdir(path.join(__dirname, 'secret-folder'), { withFileTypes: true }, (err, items) => {
     if (!err) {
         items.forEach((elem) => {
             const fileDir = path.join(__dirname, 'secret-folder', elem.name);
             if(elem.isFile()){
-                let fileSize =0;
                 fs.stat(fileDir, (err, stats) => {if (!err) {
                     console.log(path.basename(fileDir,path.extname(fileDir)) +
                      " - " +
